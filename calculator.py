210bf7beb6c07a9529bda2b927536f201d5b0d8d
Sushant Mittal <sushantmittal2002@gmail.com>
2026-03-18 11:57:57 +0530

    Removed files from GitHub only

--- calculator.py
+++
@@ -1,7 +0,0 @@
-a=10
-b=5
-print("the valuse of", a,"+",5,"is:",a+b)
-print("the valuse of", a,"-",5,"is:",a-b)
-print("the valuse of", a,"*",5,"is:",a*b)
-print("the valuse of", a,"/",5,"is:",a/b)
-
--- program1.py
+++
@@ -1,2 +0,0 @@
-print("sushant", end=' ')
-print("mittal")
